a0305ba8b140227b69c8fc83af885ec53cb5e072
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2026-03-02 13:47:05 +0000

    updated design

--- src/components/AppIcon/AppIcon.tsx
+++ src/components/AppIcon/AppIcon.tsx
@@ -8,11 +8,12 @@ interface AppIconProps {
 
 const AppIcon = ({ icon, title, path }: AppIconProps) => {
   return (
-    <a className="app-wrapper" href={path}>
-      <img src={icon} alt="tools" className="app-icon" />
-
+    <div className="icon-wrapper">
+      <a className="app-wrapper" href={path}>
+        <img src={icon} alt="tools" className="app-icon" />
+      </a>
       <p className="app-title">{title}</p>
-    </a>
+    </div>
   );
 };
 
